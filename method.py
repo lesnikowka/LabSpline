76851cead2f092dc27fdef2cab573a96209dbcfc
nik <lesnikowka@mail.ru>
2024-03-30 21:16:12 +0300

    init

---
+++ method.py
@@ -0,0 +1,164 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Тесотовая функция
+def tf(x):
+    if x >= -1 and x <= 0:
+        return x ** 3 + 3 * x ** 2
+    return -x ** 3 + 3 * x ** 2
+
+
+#Функции для основной задачи 1
+def Task1Func1(x):
+    return np.log(x + 1) / (x + 1) 
+
+
+def dTask1Func1(x): #Производная первой функции
+    return (x + (-x - 1) * np.log(x + 1)) / (x ** 3 + x ** 2)
+
+
+def d2Task1Func1(x): #Вторая производная первой функции
+    return (-np.log(x + 1) * (x ** 3 + x ** 2) - 3 * x ** 3 - 2 * x ** 2 - 3 * np.log(x + 1) * x ** 2 * (-x - 1) - 2 * np.log(x + 1) * x * (-x - 1)) / (x ** 3 + x ** 2) ** 2
+
+
+def Task1Func2(x):
+    return (x ** 0.5) * np.sin(x)
+
+
+def dTask1Func2(x): #Производная второй функции
+    return (np.sin(x) + 2 * x * np.cos(x)) / (2 * x ** 0.5)
+
+
+def d2Task1Func1(x): #Вторая производная второй функции
+    return (4 * x * np.cos(x) - 4 * x ** 2 * np.sin(x) - np.sin(x)) / (4 * x * x ** 0.5)
+
+
+#Функции для основной задачи 2
+def Task2Func1(x):
+    return np.log(x + 1) / (x + 1) + np.cos(10)
+
+
+def Task2Func2(x):
+    return (x ** 0.5) * np.sin(x) + np.cos(10)
+
+
+def initCoeff(n, h, left, right, fval):
+    Ai = h
+    Bi = h
+    Ci = -4 * h
+    kapa1 = 0
+    kapa2 = 0
+    mu1 = left
+    mu2 = right
+    phi = [0] * (n - 1)
+    for i in range(0, n - 1):   
+        phi[i] =  - 6 * ((fval[i + 2] - 2 * fval[i + 1] + fval[i]) / h) 
+    coeffs = [kapa1, kapa2, mu1, mu2, phi, Ai, Bi, Ci]
+    return coeffs
+    
+
+#Прогонка-вычисления j-го слоя
+def runTrough(coeffs, n):
+    v = [0] * (n + 1)
+    kapa1 = coeffs[0]
+    kapa2 = coeffs[1]
+    mu1 = coeffs[2]
+    mu2 = coeffs[3]
+    phi = coeffs[4]
+    Ai = coeffs[5]
+    Bi = coeffs[6]
+    Ci = coeffs[7]
+    
+    #Прямой ход
+    alpha = [0] * (n)
+    betta = [0] * (n)
+    alpha[0] = kapa1 
+    betta[0] = mu1
+    for i in range(1, n):
+        alpha[i] = Bi / (Ci - Ai * alpha[i - 1])
+        betta[i] = (phi[i - 1]  + Ai * betta[i - 1]) / (Ci - Ai * alpha[i - 1])
+    
+    #Обратный ход
+    v[n] = (-kapa2 * betta[n - 1] - mu2) / (kapa2 * alpha[n - 1] - 1)
+    for i in range(n - 1, -1, -1):
+        v[i] = alpha[i] * v[i + 1] + betta[i]
+    return v
+
+
+#Вычисление коэфициентов сплайна
+def splainCoef(a, b, n, leftBound, rightBound, f):
+    h = abs((a - b) / n)
+    x = np.linspace(a, b, n + 1)
+    funcVal = []
+    for i in range(n + 1):
+        funcVal.append(f(x[i]))
+        
+    coef = initCoeff(n, h, leftBound, rightBound, funcVal)
+    C = runTrough(coef, n)
+    
+    A = funcVal[1::]
+
+    D = []
+    for i in range(1, n + 1):
+        D.append((C[i] - C[i - 1]) / h)
+    
+    B = []
+    for i in range(1, n + 1):
+        B.append((funcVal[i] - funcVal[i - 1]) / h + C[i] * h / 3 + C[i - 1] * h / 6)
+        
+    return(A, B, C[1::], D, x)
+
+
+def splain(A, B, C, D, X, n, x):
+    for i in range(1, len(X)):
+        if x >= X[i - 1] and x < X[i]:
+            return A[i - 1] + B[i - 1] * (x - X[i]) + (C[i - 1] / 2) * ((x - X[i]) ** 2) + (D[i - 1] / 6) * ((x - X[i]) ** 3)
+        elif x == X[i]:
+            return A[i - 1]
+        
+
+N = 10 #число участков сплайна
+A = 2 #граничные условия тестовая
+B = 4
+A1 = 0.2 #граничные условия первая функция
+B1 = 2
+A2 = 1 #граничные условия вторая функция
+B2 = np.pi 
+leftBound = 0
+rightBound = 0
+
+
+xx = np.linspace(A1, B1, 2 * N + 1)
+a, b, c, d, xval = splainCoef(A1, B1, N, leftBound, rightBound, Task1Func1)
+y = []
+for i in range(len(xx)):
+    y.append(splain(a, b, c, d, xval, N, xx[i]))
+plt.plot(xx, y)
+
+yy = []
+for i in range(len(xx)):
+    yy.append(Task1Func1(xx[i]))
+plt.plot(xx, yy)
+plt.show()
+
+er = []
+for i in range(len(y)):
+    er.append(abs(yy[i] - y[i]))
+print(max(er))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
